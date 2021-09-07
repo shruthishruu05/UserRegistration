0859c2724006649746ea7190476d1a11dc61c6b9
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 22:45:08 +0530

    ADDED : method to validate the email

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -6,13 +6,18 @@ package com.bridgelabz.userregistration;
 	public class UserRegistration {
 		
 		public static boolean checkFirstName(String firstName) {
-			Pattern pattern = Pattern.compile("^[a-zA-Z]{2,}$");
+			Pattern pattern = Pattern.compile("^[a-zA-Z]{3,}$");
 			Matcher matcher = pattern.matcher(firstName);
 			return matcher.matches();
 		}
 		public static boolean checkLastName(String lastName) {
 			Pattern pattern = Pattern.compile("^[a-zA-Z]$");
 			Matcher matcher = pattern.matcher(lastName);
+			return matcher.matches();
+		}
+		public static boolean checkEmail(String email) {
+			Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9][a-zA-Z0-9_-]*)(([+_.-][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]+)([.])([a-z]{2,})(([.][a-z]{2,})?)");
+			Matcher matcher = pattern.matcher(email);
 			return matcher.matches();
 		}
 		
@@ -26,6 +31,10 @@ package com.bridgelabz.userregistration;
 				if(checkLastName(scanner.nextLine())) System.out.println("valid");
 				else System.out.println("Invalid");
 				
+				System.out.println("enter email:");
+				String email = scanner.nextLine();
+				if(checkEmail(email)) System.out.println("valid");
+				else System.out.println("Invalid");
 				scanner.close();
 		}
 	}

22d15450ce89097171a890662a905e698e01712a
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 23:19:09 +0530

    ADDED : method to validate the phone number with atleast one numeric value

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -25,7 +25,7 @@ package com.bridgelabz.userregistration;
 			return matcher.matches();
 		}
 		public static boolean checkPassword(String password) {
-			Pattern pattern = Pattern.compile("^(?=.*[A-Z])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$");
+			Pattern pattern = Pattern.compile("^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$");
 			Matcher matcher = pattern.matcher(password);
 			return matcher.matches();
 		}
